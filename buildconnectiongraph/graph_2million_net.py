efa0b42a3fb1c81de54b68b49390a1cd932d640b
yuehuca <37157431+yuehuca@users.noreply.github.com>
2021-10-30 01:07:44 -0400

    Add files via upload

---
+++ buildconnectiongraph/graph_2million_net.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from pyecharts import options as opts
+from pyecharts.charts import Graph
+
+df_links = pd.read_csv("links.csv")
+df_categories = pd.read_csv("categories.csv")
+df_nodes = pd.read_csv("nodes.csv")
+
+links =  df_links.to_dict('records')
+categories = df_categories.to_dict('records')
+nodes =  df_nodes.to_dict('records')
+
+for item_nodes in nodes:
+    item_nodes['label'] = {'normal':{'show':'True'}}
+
+c = (
+    Graph(init_opts=opts.InitOpts(width="2000px", height="1200px"))
+    .add(
+        "",
+        nodes,
+        links,
+        categories,
+        repulsion=50,
+        linestyle_opts=opts.LineStyleOpts(curve=0.2),
+        label_opts=opts.LabelOpts(is_show=False),
+    )
+    .set_global_opts(
+        legend_opts=opts.LegendOpts(orient="vertical", pos_left="2%", pos_top="20%"),
+        title_opts=opts.TitleOpts(title="High Net Worth Canadian Executives Connection Graph"),
+    )
+    .render("graph_2.html")
+)
+
+
